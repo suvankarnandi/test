78cfe2ad28c4e232ef2c1883de156fdcc8ce7e19
suvankar nandi <suvankarnandi@gmail.com>
2025-03-31 22:12:53 +0530

    Second code

--- FirstPython.py
+++ FirstPython.py
@@ -1,4 +1,6 @@
-name = "suvankar"
-age = 39
-print(name, age)
-print(type(name), type(age))
+sonai = 10
+mithai = 7
+print ("sonai is elder than mithai: ",(sonai>mithai))
+print ("combind age of both : ", (sonai+mithai))
+print ("Mithai age is 7Yrs. then what is the age of Sonai ?", 
+       "ans :-", 17 - mithai)
