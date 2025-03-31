a1bc2689c307f0b52e363e5d7255852b6e84013a
suvankar nandi <suvankarnandi@gmail.com>
2025-03-31 22:26:12 +0530

    3rd code

--- FirstPython.py
+++ FirstPython.py
@@ -4,3 +4,8 @@ print ("sonai is elder than mithai: ",(sonai>mithai))
 print ("combind age of both : ", (sonai+mithai))
 print ("Mithai age is 7Yrs. then what is the age of Sonai ?", 
        "ans :-", 17 - mithai)
+
+#3rd code is here
+val1 = True
+val2 = False
+print ( val1 or val2)
