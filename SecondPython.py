5a9696d759fb5856e38b090197f8864bee6fc7e6
suvankar nandi <suvankarnandi@gmail.com>
2025-04-04 20:29:06 +0530

    Conditional Statement code

--- SecondPython.py
+++ SecondPython.py
@@ -1,1 +1,21 @@
 # Second cahpter in Python
+name = str(input("what is your name : "))
+
+print(name, len(name))
+print(name.count("a"))
+
+# Conditional Statement
+exp = int(input("how much exp. in Networking: "))
+
+if(exp>=15 and exp<=20):
+    Designation = "You are eligible for Operation Manger"
+elif(exp>=10 and exp<15):
+    Designation = "You are eligible for Team Lead"
+elif(exp>=5 and exp<10):
+    Designation = "You are eligible for Network Engineer"
+elif(exp>1 and exp<5):
+    Designation = "You are eligible for Associate Engineer"
+else:
+    Designation = "Sorry, you are not eligible"
+
+print(Designation)
