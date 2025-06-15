b5a2b541037525496fbf7a222e18014c17fd932d
suvankar nandi <suvankarnandi@gmail.com>
2025-06-15 20:01:00 +0530

    testes code

--- while-loop.py
+++ while-loop.py
@@ -14,14 +14,15 @@
 #    i +=1
 
 
-mobile = [8443910056, 9674423685, 9239417975, 9239381348]
+mobile = (8443910056, 9674423685, 9239417975, 8240129420, 9239381348 )
 
-number = int(input("your mobile number: "))
+number = int(input("put your number:"))
 id = 0
 while id < len(mobile):
     if (mobile[id] == number):
-        print("found your number : ", id)
-        # break
+        print("found your number in the list ", id)
+        break
+
     else: 
         print ("number not listed.")
     id +=1
