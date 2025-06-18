15f42d6581a8aef8074e5093fe6fb2a10d309794
suvankar nandi <suvankarnandi@gmail.com>
2025-06-18 21:47:54 +0530

    first for loop

--- while-loop.py
+++ while-loop.py
@@ -14,10 +14,10 @@
 #    i +=1
 
 
-mobile = (8443910056, 9674423685, 9239417975, 8240129420, 9239381348 )
+# mobile = (8443910056, 9674423685, 9239417975, 8240129420, 9239381348 )
 
-number = int(input("put your number:"))
-id = 0
+# number = int(input("put your number:"))
+# id = 0
 # while id < len(mobile):
 #     if (mobile[id] == number):
 #         print("found your number in the list ", id)
@@ -27,13 +27,36 @@ id = 0
 #         print ("number not listed.")
 #     id +=1
     
-while id < len(mobile):
-    if (mobile[id] == number):
-        id +=1
-        print("found your number in the list ", id)
-        continue
+# while id < len(mobile):
+#     if (mobile[id] == number):
+#         id +=1
+#         print("found your number in the list ", id)
+#         continue
 
-    else: 
-        print ("number not listed.")
-    id +=1
+#     else: 
+#         print ("number not listed.")
+#     id +=1
     
+#For Loop
+# nums = [1, 4, 9, 16, 25, 36]
+
+# for value in nums:
+#     print(value)
+
+roll =(5, 8, 7, 85, 58, 75, 57,)
+
+XX = int(input("what is your roll N0. :"))
+
+i = 0
+
+for el in roll:
+    if (el == XX):
+        print("you are pass and rank:", i)
+        break
+    i += 1
+else:
+    print("sorry, you are fail.")
+        
+
+
+
